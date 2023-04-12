aecd58ffc69af65d1cb49ab3d6696ab90639e351
eukkki210 <ysg980423@gmail.com>
2023-04-12 13:36:35 +0900

    Update ssl_test.py

--- ssl_test.py
+++ ssl_test.py
@@ -10,8 +10,21 @@ if __name__ == '__main__':
     ll.append(325)
     ll.first()
     ll.traverse_all() 
-# 예상 출력: head -> (100) -> (72) -> (325) -> null 
-# 실제 출력: head ->
-# ssl.py가 아닌 Python 내장 모듈 중 하나인 'ssl' 모듈을 임포트한 것 같음
-# traverse_all 메소드 실행 전에 first 메소드를 호출해야됨.
-# 실제 출력: head ->(100) -> (72) -> (325) -> null
+    # 예상 출력: head -> (100) -> (72) -> (325) -> null 
+    # 실제 출력: head ->
+    # ssl.py가 아닌 Python 내장 모듈 중 하나인 'ssl' 모듈을 임포트한 것 같음 -> 파일 명 myssl.py로 변경
+    # traverse_all 함수 실행 전에 first 함수를 호출해야됨.
+    # 실제 출력: head ->(100) -> (72) -> (325) -> null
+
+    # insert_at() 함수 테스트
+    ll.insert_at(2, 500)
+    ll.first()
+    ll.traverse_all() 
+    # 예상 출력: head -> (100) -> (500) -> (72) -> (325) -> null
+    # 실제 출력: head ->(100) -> (500) -> (72) -> (325) -> null
+
+    # remove() 함수 테스트
+    ll.remove(72)
+    ll.first()
+    ll.traverse_all()
+    # 예상 출력: (100) -> (500) -> (325) -> null
