cc46146d129a3c217b9cd3d60181614c4921e58d
eukkki210 <ysg980423@gmail.com>
2023-04-12 13:21:33 +0900

    Update ssl_test.py

--- ssl_test.py
+++ ssl_test.py
@@ -1,15 +1,14 @@
 # 테스트 코드
-import ssl
+from ssl import *
 
-ll = ssl.LinkedList()
+if __name__ == '__main__':
+    ll = LinkedList()
 
-ll.first()
-
-# append() 함수 테스트
-ll.append(100)
-ll.append(72)
-ll.append(325)
-ll.traverse_all() 
+    # append() 함수 테스트
+    ll.append(100)
+    ll.append(72)
+    ll.append(325)
+    ll.traverse_all() 
 # 예상 출력: head -> (100) -> (72) -> (325) -> null 
 # 실제 출력: head ->
-# first를 실행하지 않아서 발생한 문제로 보임 -> 아닌듯
+# ssl.py가 아닌 Python 내장 모듈 중 하나인 'ssl' 모듈을 임포트한 것 같음
