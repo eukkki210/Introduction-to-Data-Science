3809865d8c00d7805927d2f603ed86eaaed5d6ce
eukkki210 <ysg980423@gmail.com>
2023-04-12 13:13:25 +0900

    Update ssl_test.py

--- ssl_test.py
+++ ssl_test.py
@@ -3,6 +3,8 @@ import ssl
 
 ll = ssl.LinkedList()
 
+ll.first()
+
 # append() 함수 테스트
 ll.append(100)
 ll.append(72)
@@ -10,4 +12,4 @@ ll.append(325)
 ll.traverse_all() 
 # 예상 출력: head -> (100) -> (72) -> (325) -> null 
 # 실제 출력: head ->
-# first를 실행하지 않아서 발생한 문제로 보임
+# first를 실행하지 않아서 발생한 문제로 보임 -> 아닌듯
