40e2576eb83bcda6b582bae8b776b230c3c39511
eukkki210 <ysg980423@gmail.com>
2023-04-12 13:28:27 +0900

    Rename ssl.py and Update ssl_test.py

--- ssl_test.py
+++ ssl_test.py
@@ -1,5 +1,5 @@
 # 테스트 코드
-from ssl import *
+from myssl import *
 
 if __name__ == '__main__':
     ll = LinkedList()
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     ll.append(100)
     ll.append(72)
     ll.append(325)
+    ll.first()
     ll.traverse_all() 
 # 예상 출력: head -> (100) -> (72) -> (325) -> null 
 # 실제 출력: head ->
 # ssl.py가 아닌 Python 내장 모듈 중 하나인 'ssl' 모듈을 임포트한 것 같음
+# traverse_all 메소드 실행 전에 first 메소드를 호출해야됨.
+# 실제 출력: head ->(100) -> (72) -> (325) -> null
