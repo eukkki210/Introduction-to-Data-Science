0c4576288d02af2df3c879de1aee83f98bca7d33
eukkki210 <ysg980423@gmail.com>
2023-04-13 23:38:43 +0900

    Update myssl.py

--- myssl.py
+++ myssl.py
@@ -45,8 +45,10 @@ class LinkedList:
 
 		self.num_of_data -= 1
 		
-		if self.current == None:
-			self.first()
+		if self.current != None:
+			self.current = self.current.next
+		
+		self.first()
 
 		return pop_data
 
@@ -82,6 +84,7 @@ class LinkedList:
 	# traverse_all 메소드 (search3 - 순차적으로 모든 원소 탐색, 각 노드의 data print)
 	def traverse_all(self):
 		if self.num_of_data == 0:
+			print("빈 리스트입니다.")
 			return None
 		
 		# 현재 node가 None이면 처음 노드로 이동
@@ -104,66 +107,68 @@ class LinkedList:
 	# insert_at 메소드 (insert2 - 주어진 position에 노드 추가, before과 node의 next, 데이터 개수 변경)
 	def insert_at(self, position, new_data):
 		# position이 1보다 작은 경우 메소드 종료 (error)
-		if position < 1 or position > self.num_of_data + 1:
-			print("오류: position은 1보다 크거나 같고 전체 데이터 수보다 작거나 같아야 합니다.")
+		if position < 1:
+			print("오류: position은 1보다 크거나 같아야 합니다.")
 			return None
-		# position이 num_of_data보다 큰 경우, append 메소드를 호출하여 맨 마지막에 new_data 삽입
+		# position이 num_of_data보다 큰 경우
 		elif position > self.num_of_data:
 			self.append(new_data)
-			return None
 		# 유효한 position의 경우
-		new_node = Node(new_data)
-		# 현재 node 탐색 (현재 위치 1로 초기화)
-		cur_pos = 1
-		cur_node = self.current
-		# 현재 위치가 position보다 작은 동안 반복 (반복문 내부에서 before, current 갱신)
-		while cur_pos < position:
-			self.before = cur_node
-			self.current = cur_node.next
-			cur_pos += 1
-		# position이 현재 LinkedList에 존재하는 node의 위치 범위에서 벗어나는 경우
-		if cur_pos < position:
-			print("오류: 위치가 올바르지 않습니다.")
-			return None
+		else:
+			new_node = Node(new_data)
+			# 현재 node 탐색 (현재 위치 1로 초기화)
+			cur_pos = 1
+			cur_node = self.current
+			# 현재 위치가 position보다 작은 동안 반복 (반복문 내부에서 before, current 갱신)
+			while cur_pos < position:
+				self.before = cur_node
+				self.current = cur_node.next
+				cur_pos += 1
+			# position이 현재 LinkedList에 존재하는 node의 위치 범위에서 벗어나는 경우
 
-		self.before.next = new_node
-		new_node.next = self.current
+			self.before.next = new_node
+			new_node.next = self.current
 
-		self.first()
+			self.first()
 
-		self.num_of_data += 1
+			self.num_of_data += 1
 
 	# remove 메소드 (delete2 - key와 일치하는 node 삭제, 인접 node의 current, next 변경, 데이터 개수 변경)
 	def remove(self, key):
 		# 처리 결과 출력을 위해 삭제된 원소 개수 카운트
 		del_cnt = 0
 		# 몇 번째 원소인지 알기 위해 idx 변수 선언
-		idx = 0
+		idx = 1
 		# 빈 리스트일 경우 메소드 종료
 		if self.num_of_data == 0:
-			print("해당하는 원소가 없습니다.")
+			print("빈 리스트입니다.")
 			return None
-		cur_node = self.current
+		
+		cur_node = self.head
 		# 리스트 전체 탐색 (cur_node가 None이 되면 끝까지 탐색한 것)
-		while cur_node != None:
+		while cur_node:
 			# 삭제할 node 발견
 			if cur_node.data == key:
 				# 삭제할 node가 첫 번째 node일 경우
-				if cur_node == self.current:
-					self.current = cur_node.next
-					cur_node = self.current
+				if cur_node == self.head:
+					self.head = cur_node.next
+					cur_node = self.head					
 				# 삭제할 node가 중간에 위치하는 경우
 				else:
 					self.before.next = cur_node.next
 					cur_node = cur_node.next
+
+				# 현재 node가 tail node일 경우 tail을 재설정
+				if cur_node == self.tail:
+					self.tail = self.before
 					
 				self.num_of_data -= 1
 				del_cnt += 1
-				# 삭제된 node의 inex를 기록하는 변수를 사용하여 출력
-				print(f"{idx}번째 원소(key)를 삭제합니다.")
-				break
-			# 현재 node의 값이 key와 같지 않다면, 다믐 node로 이동
+				# 삭제된 node의 index를 기록하는 변수를 사용하여 출력
+				print(f"{idx-1}번째 원소({key})를 삭제합니다.")
+				continue
 			else:
+			# 현재 node의 값이 key와 같지 않다면, 다믐 node로 이동
 				self.before = cur_node
 				cur_node = cur_node.next
 			idx += 1
